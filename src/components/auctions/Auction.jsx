625069ce532bbf6d7ee081554c85446d96c3d761
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-03 15:25:48 +0600

    final understanding of logic in state

--- src/components/auctions/Auction.jsx
+++ src/components/auctions/Auction.jsx
@@ -5,7 +5,7 @@ import { RxCross1 } from "react-icons/rx";
 
 const Auction = () => {
     const [bids, setBids] = useState([]);
-    console.log(bids)
+    const [favorite, setFavorite] = useState([])
 
     useEffect(() => {
         fetch("/data.json")
@@ -13,11 +13,14 @@ const Auction = () => {
             .then((res) => setBids(res));
     }, [])
 
-    const [secondDiv, setSecondDiv] = useState({})
     const handleLeft = (bid) => {
-        setSecondDiv(bid)
+        const finalItem = favorite.find(item => item.id === bid.id)
+
+        if (!finalItem) {
+            setFavorite([...favorite, bid])
+        }
     }
-    const { currentBidPrice, image, timeLeft, title, id, bidsCount } = secondDiv
+
     return (
         <div className="min-h-screen py-10 space-y-4">
             <div className="flex flex-col items-center justify-center gap-3.5 ">
@@ -39,7 +42,12 @@ const Auction = () => {
                         </thead>
                         <tbody>
                             {
-                                bids.map(bid => <Tr key={bid.id} bid={bid} handleLeft={handleLeft}></Tr>)
+                                bids.map(bid =>
+                                    <Tr
+                                        key={bid.id}
+                                        bid={bid}
+                                        handleLeft={handleLeft}>
+                                    </Tr>)
                             }
                         </tbody>
                     </table>
@@ -52,45 +60,16 @@ const Auction = () => {
                     </div>
                     <hr className="text-blue-500 md:mt-2" />
 
-                    {image ?
-                        <div className="flex items-center gap-2 justify-center">
-                            <div className="flex m-2">
-                                <img className="w-22 h-18 object-cover rounded-xl border" src={image} alt="" />
-                            </div>
-                            <div>
-                                <h3 className="text-lg font-semibold">{title}</h3>
-                                <p className="">${currentBidPrice}</p>
-                                <p>Bids: {bidsCount}</p>
-                            </div>
-                            <button className="text-red-500 font-extrabold text-xl">X</button>
-                        </div>
-                        : <div className="text-center m-5 text-orange-500">
-                            <h3>No favorite yet</h3>
-                            <p>Click the heart icon on any Item to add it to our favorite</p>
-                            <hr />
-                        </div>}
-                    {/* <table>
-                        <thead>
-                            <tr>
-                                <td><img className="w-24" src={image} alt="" /></td>
-                                <td>{`${title, bidscount}`}</td>
-                                <td>{}<RxCross1 /></td>
-                            </tr>
-                        </thead>
-                    </table> */}
-                    {/* <p>{id}</p>
-                    <p>{currentBidPrice}</p>
-                    <p>{timeLeft}</p>
-                    <img src={image} alt="" />
-                    <p>{title}</p> */}
-                    {/* {image ? (
-                        <div className="flex items-center gap-4 bg-gray-100 p-3 rounded-lg shadow">
-                            <img className="w-24 h-20 object-cover rounded" src={image} alt="" />
-
+                    {favorite ? (favorite.map(fav => (
+                        <div className="flex items-center gap-4 bg-gray-100 p-3 rounded-lg shadow m-2">
+                            <img className="w-24 h-20 object-cover rounded" src={fav.image} alt="" />
                             <div className="flex-1">
-                                <h3 className="font-semibold">{title}</h3>
+                                <h3 className="font-semibold">{fav.title}</h3>
                                 <p className="text-sm text-gray-600">
-                                    Bids: {bidsCount}
+                                    $ {fav.currentBidPrice}
+                                </p>
+                                <p className="text-sm text-gray-600">
+                                    Bids: {fav.bidsCount}
                                 </p>
                             </div>
 
@@ -98,9 +77,13 @@ const Auction = () => {
                                 <RxCross1 size={20} />
                             </button>
                         </div>
-                    ) : (
-                        <p className="text-gray-400 text-center">No favorite selected</p>
-                    )} */}
+                    ))) : (
+                        <div>
+                            <p className="text-gray-400 text-center">No favorite selected</p>
+                            <p className="text-sm mb-3">Click the heart icon on any item to add it to your favorite</p>
+                            <hr className="border-orange-300" />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
